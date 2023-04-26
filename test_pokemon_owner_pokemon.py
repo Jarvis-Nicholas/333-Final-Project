8428fa02512892ef3560d8510d28295e671512c8
Jarvis-Nicholas <njarvis@nevada.unr.edu>
2023-04-26 01:40:03 -0700

    need more methods to build better integration test

---
+++ test_pokemon_owner_pokemon.py
@@ -0,0 +1,28 @@
+"""
+Author: Nicholas Jarvis
+CS 333 Final Project: Pokemon
+Date: 4/25/2023
+"""
+
+# Command line executables:
+# python3 -m coverage run test_pokemon_owner_pokemon.py -v -b
+# python3 -m coverage report 
+
+import unittest
+import pokemon as pk
+import pokemon_owner as po
+
+class testPokemonOwnerPokemon(unittest.TestCase):
+    def setUp(self):
+        self.owner = po.PokemonOwner("name")
+        self.pokemon = pk.Pokemon("name", "type", 10)
+        self.owner.add_pokmeon(self.pokemon)
+
+    def test_pokemon_name(self):
+       self.assertEqual(self.owner.team[0].name, self.pokemon.name)
+    
+    def test_pokmeon_type(self):
+        self.assertEqual(self.owner.team[0].type, self.pokemon.type)
+
+    def test_pokemon_health(self):
+        self.assertEqual(self.owner.team[0].health, self.pokemon.health)
